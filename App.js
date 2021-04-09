c46d90b9310016dcef3a47d0aee2f743cb251247
nguyenthangm49yt <nguyenthangm49yt@gmail.com>
2021-04-09 22:44:26 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { SplashScreen } from './src/screens/SplashScreen/SplashScreen';
+import { SignUp } from './src/screens/SignUp/SignUp';
+import { Login } from './src/screens/Login/Login';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    <SignUp/>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    //backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
