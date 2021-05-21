ed57fd137c185e478263d899b0614d026324a894
DAI-svg-source <68337319+DAI-svg-source@users.noreply.github.com>
2021-05-21 16:17:31 +0700

    edit profile

--- src/screens/EditProfile/EditProfile.js
+++ src/screens/EditProfile/EditProfile.js
@@ -1,102 +1,123 @@
 import React from 'react';
-import {View, Text, StyleSheet, Image, SafeAreaView, ScrollView,TouchableOpacity, Alert} from 'react-native';
-import {colors} from '../../config/colors';
+import { View, Text, StyleSheet, Image, SafeAreaView, ScrollView, TouchableOpacity, Alert } from 'react-native';
+import { colors } from '../../config/colors';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import {styles} from './styles'
-import {EditForm} from '../../components/forms//EditForm/EditForm';
+import { styles } from './styles'
+import { EditForm } from '../../components/forms//EditForm/EditForm';
+import useAxios from 'axios-hooks'
 
+const InfoBox = ({ title, placeHolder, value }) => {
 
-const InfoBox = ({title,placeHolder,value}) => {
-
-    return(
+    return (
         <View>
             {/* title of infoBox */}
             <View style={styles.titleForm}>
-                <Text style={{fontSize:18}}>{title}</Text>
+                <Text style={{ fontSize: 18 }}>{title}</Text>
             </View>
 
             {/* info */}
             <View style={styles.infoBox}>
                 <View style={styles.form}>
-                    <EditForm 
+                    <EditForm
                         placeHolder={placeHolder}
-                        
+
                         value={value}
                     />
                 </View>
-                   
-                        
+
+
             </View>
         </View>
     )
 }
 
-export default class EditProfile extends React.Component {
+export default function EditProfile () {
+    const[
+        { data: putData, loading: putLoading, error: putError },
+        executePost
+    ] = useAxios(
+        {
+            url: `${URL}/api/auth/edit-profile`,
+            method: 'POST',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+        },
+        { manual: true }
+    )
+
     handleBack = () => {
         // TODO:.
         this.props.navigation.goBack();
-      }
+    }
     handleSave = () => {
         // TODO: 
         Alert.alert(
             "Save",
             "Are you sure?",
             [
-            {
-                text: "Cancel",
-                onPress: () => console.log("Cancel Pressed"),
-                style: "cancel"
-            },
-            { text: "OK", onPress: () => console.log("OK Pressed") }
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                {
+                    text: "OK", onPress: () => {
+                        // executePost({
+                        //     data: {
+                        //         username, email, password, password_confirmation: passwordConfirmation
+                        //     }
+                        // })
+                    }
+                }
             ]
         );
-      }
-    
-    render(){
-    return (
-        <SafeAreaView style={styles.container}>
-            {/* Headers */}
-            <View style={styles.header}>
-                <TouchableOpacity style={styles.headerRightBtnsWrapper}
-                onPress={this.handleBack}>
-                    
-                    <MaterialCommunityIcons name="arrow-left-circle-outline" color="#36454f" size={35} />
-                </TouchableOpacity>  
-                <Text style={styles.headerTitle}>Edit your Profile</Text>
-                <TouchableOpacity 
-                style={styles.headerRightBtnsWrapper}
-                onPress={this.handleSave}>
-               
-                    <MaterialCommunityIcons name="content-save-edit-outline" color="#36454f" size={30} />
-                </TouchableOpacity>
-            </View>
+    }
 
-             {/* content */}
-            <ScrollView showsVerticalScrollIndicator={false}
-                contentContainerStyle={{paddingBottom:30}}>
-                {/* user name and email */}
-                <View style={styles.userWrapper}>
-                     
-                        <Image style={styles.image} source={require('../../../assets/images/user.png')}/>
-                    
-                    <View style={styles.profileInfos}>
-                        <View style={styles.nameSection}>
-                            <Text style={styles.nameSectionAcc}>Name</Text>
-                            <Text style={{color: colors.white}}>name@gmail.com</Text>
-                        </View>
-                    </View>
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                {/* Headers */}
+                <View style={styles.header}>
+                    <TouchableOpacity style={styles.headerRightBtnsWrapper}
+                        onPress={this.handleBack}>
+
+                        <MaterialCommunityIcons name="arrow-left-circle-outline" color="#36454f" size={35} />
+                    </TouchableOpacity>
+                    <Text style={styles.headerTitle}>Edit your Profile</Text>
+                    <TouchableOpacity
+                        style={styles.headerRightBtnsWrapper}
+                        onPress={this.handleSave}>
+
+                        <MaterialCommunityIcons name="content-save-edit-outline" color="#36454f" size={30} />
+                    </TouchableOpacity>
                 </View>
 
-                  {/* info list of user */}
-                 <InfoBox title={'Name'} placeHolder={'name'} value={'username'} />
-                 <InfoBox title={'Email'} placeHolder={'name@gmail.com'} value={'name@gmail.com'} />
-                 <InfoBox title={'Phone'} placeHolder={'Phone'} value={'0123456789'} />
-                 
-               
-                
-                
-            </ScrollView>
-        </SafeAreaView>
-    );
+                {/* content */}
+                <ScrollView showsVerticalScrollIndicator={false}
+                    contentContainerStyle={{ paddingBottom: 30 }}>
+                    {/* user name and email */}
+                    <View style={styles.userWrapper}>
+
+                        <Image style={styles.image} source={require('../../../assets/images/user.png')} />
+
+                        <View style={styles.profileInfos}>
+                            <View style={styles.nameSection}>
+                                <Text style={styles.nameSectionAcc}>Name</Text>
+                                <Text style={{ color: colors.white }}>name@gmail.com</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    {/* info list of user */}
+                    <InfoBox title={'Name'} placeHolder={'name'} value={'username'} />
+                    <InfoBox title={'Phone'} placeHolder={'Phone'} value={'0123456789'} />
+
+
+
+
+                </ScrollView>
+            </SafeAreaView>
+        );
     }
 };
